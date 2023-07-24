b5eec174bee88590b09f671f63af02123ae16ec2
y86-dev <y86-dev@protonmail.com>
2023-07-24 23:33:50 +0200

    impl `Zeroable` for `UnsafeCell<T>`

--- src/lib.rs
+++ src/lib.rs
@@ -259,6 +259,7 @@ extern crate alloc;
 use alloc::{boxed::Box, sync::Arc};
 use core::{
     alloc::AllocError,
+    cell::UnsafeCell,
     convert::Infallible,
     marker::PhantomData,
     mem::MaybeUninit,
@@ -1384,6 +1385,9 @@ impl_zeroable! {
 
     // SAFETY: Type is allowed to take any value, including all zeros.
     {<T>} MaybeUninit<T>,
+
+    // SAFETY: `T: Zeroable` and `UnsafeCell` is `repr(transparent)`.
+    {<T: ?Sized + Zeroable>} UnsafeCell<T>,
 
     // SAFETY: All zeros is equivalent to `None` (option layout optimization guarantee).
     Option<NonZeroU8>, Option<NonZeroU16>, Option<NonZeroU32>, Option<NonZeroU64>,
