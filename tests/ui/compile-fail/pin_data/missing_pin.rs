2a8618d6283fef15df168275d527c9b9551d1045
y86-dev <y86-dev@protonmail.com>
2024-04-11 20:02:23 +0200

    tests: ui: compile-fail: pin_data: add missing_pin

---
+++ tests/ui/compile-fail/pin_data/missing_pin.rs
@@ -0,0 +1,18 @@
+#![feature(allocator_api)]
+extern crate pinned_init;
+use pinned_init::*;
+
+#[pin_data]
+struct Foo {
+    a: usize,
+}
+
+impl Foo {
+    fn new(a: impl PinInit<usize>) -> impl PinInit<Self> {
+        pin_init!(Self {
+            a <- a,
+        })
+    }
+}
+
+fn main() {}
