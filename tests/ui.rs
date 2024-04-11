eb9780176fe38c0aee261206a7952b49ea84fe3b
y86-dev <y86-dev@protonmail.com>
2024-04-12 01:32:17 +0200

    tests: ui: create subdirectories for the different macros

--- tests/ui.rs
+++ tests/ui.rs
@@ -1,4 +1,7 @@
 #[cfg_attr(not(any(miri, NO_UI_TESTS)), test)]
-fn ui() {
-    trybuild::TestCases::new().compile_fail("tests/ui/*.rs");
+fn compile_fail() {
+    let test_cases = trybuild::TestCases::new();
+    test_cases.compile_fail("tests/ui/compile-fail/pinned_drop/*.rs");
+    test_cases.compile_fail("tests/ui/compile-fail/pin_data/*.rs");
+    test_cases.compile_fail("tests/ui/compile-fail/init/*.rs");
 }
