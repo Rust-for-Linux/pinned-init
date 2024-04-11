f89fcef6cb9a302ff10b0e84b85e2f660079400a
y86-dev <y86-dev@protonmail.com>
2024-04-11 22:22:30 +0200

    tests: ui: compile-fail: init: add missing_comma_with_zeroable

---
+++ tests/ui/compile-fail/init/missing_comma_with_zeroable.rs
@@ -0,0 +1,15 @@
+#![feature(allocator_api)]
+extern crate pinned_init;
+use pinned_init::*;
+
+#[derive(Zeroable)]
+struct Foo {
+    a: usize,
+    b: usize,
+}
+
+fn main() {
+    let _ = init!(Foo {
+        a: 0..Zeroable::zeroed()
+    });
+}
