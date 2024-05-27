7413d9caf269db63c5164a3befedc1a0b21b03bf
y86-dev <y86-dev@protonmail.com>
2024-05-27 10:11:35 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -4,33 +4,37 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
   outputs = {
     self,
     fenix,
-    flake-utils,
     nixpkgs,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages.default = let
-        toolchain = fenix.packages.${system}.minimal.toolchain;
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        (pkgs.makeRustPlatform {
-          cargo = toolchain;
-          rustc = toolchain;
-        })
-        .buildRustPackage {
-          pname = "pinned-init";
-          version = "0.0.7";
-
-          src = ./.;
-
-          cargoLock.lockFile = ./Cargo.lock;
-        };
-    });
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      overlays = [fenix.overlays.default];
+      inherit system;
+    };
+  in {
+    packages."${system}".default = fenix.packages."${system}".minimal.toolchain;
+    devShells."${system}".default = pkgs.mkShell {
+      name = "rust";
+      packages = with pkgs; [
+        (fenix.packages."${system}".complete.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+        ])
+        cargo-expand
+        cargo-rdme
+        cargo-semver-checks
+        rust-analyzer-nightly
+      ];
+      shellHook = ''LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib" exec fish'';
+    };
+  };
 }
-
