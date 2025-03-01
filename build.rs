cbc6acbe2a2410b11b454378afd40569c179c99a
y86-dev <y86-dev@protonmail.com>
2025-03-01 13:00:43 +0100

    conditionally enable the `new_uninit` feature on older nightly compilers

--- build.rs
+++ build.rs
@@ -2,9 +2,13 @@ use rustc_version::{version, Version};
 
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(RUSTC_LINT_REASONS_IS_STABLE)");
+    println!("cargo::rustc-check-cfg=cfg(RUSTC_NEW_UNINIT_IS_STABLE)");
     if version().unwrap() >= Version::parse("1.81.0").unwrap()
         || version().unwrap() >= Version::parse("1.81.0-nightly").unwrap()
     {
         println!("cargo:rustc-cfg=RUSTC_LINT_REASONS_IS_STABLE");
+    }
+    if version().unwrap() >= Version::parse("1.82.0").unwrap() {
+        println!("cargo:rustc-cfg=RUSTC_NEW_UNINIT_IS_STABLE");
     }
 }
