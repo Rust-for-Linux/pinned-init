3e9a1225493dfad66286cd238516a0f11179fa36
y86-dev <y86-dev@protonmail.com>
2025-03-01 12:02:25 +0100

    conditionally enable the `lint_reasons` feature on older nightly compilers

--- build.rs
+++ build.rs
@@ -1,1 +1,10 @@
-fn main() {}
+use rustc_version::{version, Version};
+
+fn main() {
+    println!("cargo::rustc-check-cfg=cfg(RUSTC_LINT_REASONS_IS_STABLE)");
+    if version().unwrap() >= Version::parse("1.81.0").unwrap()
+        || version().unwrap() >= Version::parse("1.81.0-nightly").unwrap()
+    {
+        println!("cargo:rustc-cfg=RUSTC_LINT_REASONS_IS_STABLE");
+    }
+}
